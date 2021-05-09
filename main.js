89d4e7597d2a5b0119280b2936659a780d204d84
chaoocharles <charleykairu@gmail.com>
2021-05-09 16:07:50 +0300

    js fomart

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 var slideIndex = 1;
 function showSlides() {
-	var slides = document.getElementsByClassName("mySlides");
-	if (slideIndex > slides.length) {slideIndex = 1;}
-	if (slideIndex < 1) {slideIndex = slides.length;}
-	for (var i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	slides[slideIndex-1].style.display = "block";
- }
+  var slides = document.getElementsByClassName("mySlides");
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
 function plusSlides(n) {
-	slideIndex += n;
-	showSlides();
+  slideIndex += n;
+  showSlides();
 }
 function currentSlide(n) {
-	slideIndex = n;
-	showSlides();
+  slideIndex = n;
+  showSlides();
 }
+
 window.onload = showSlides;
