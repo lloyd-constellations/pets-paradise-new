ed2afdd9a4a0d31e9490c65ac89af01eabd82d8c
chaoocharles <charleykairu@gmail.com>
2021-05-18 14:35:35 +0300

    gallery

--- gallery/main.js
+++ gallery/main.js
@@ -3,3 +3,31 @@ const selectElement = (s) => document.querySelector(s);
 selectElement(".menu-icon").addEventListener("click", () => {
   selectElement(".nav-items").classList.toggle("active");
 });
+
+// Gallery tabs
+// Select elements
+const buttons = document.querySelectorAll(".gallery-tab-cta");
+const gallery = document.querySelectorAll(".gallery");
+
+// function
+function switchTab(button) {
+  gallery.forEach((gallery, index) => {
+    if (gallery.id === button.id + "Content") {
+      gallery.style.display = "grid";
+      button.classList.add("active");
+      gallery.style.animation = `fadeIn 1s`;
+    } else {
+      gallery.style.display = "none";
+      // note.style.animation = "";
+    }
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].classList.remove("active");
+    }
+    button.classList.add("active");
+  });
+}
+
+// add event listeners to each button
+buttons.forEach((button) => {
+  button.addEventListener("click", () => switchTab(button));
+});
